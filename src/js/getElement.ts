092abf09b98852deae3d1c9127ed0ea535933393
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-07-12 01:51:02 +0900

    [ADD] 解析後に顔枠表示を追加

---
+++ src/js/getElement.ts
@@ -0,0 +1,4 @@
+export const container = document.querySelector('.canvas-container')
+export const canvas = document.querySelector('.canvas')
+// @ts-ignore
+export const context = canvas.getContext('2d')
--- src/js/inputImgPreview.ts
+++ src/js/inputImgPreview.ts
@@ -1,10 +1,10 @@
-const defaultImgPath = 'https://firebasestorage.googleapis.com/v0/b/facespot-b3ab6.appspot.com/o/default_img.svg?alt=media&token=8bf92214-6896-465f-97ce-125b5902691d'
-const container = document.querySelector('.canvas-container')
-const canvas = document.querySelector('.canvas')
-// @ts-ignore
-const context = canvas.getContext('2d')
+import {container, canvas, context} from "./getElement";
 
-const drawImage = (imgPath) => {
+const defaultImgPath = 'https://firebasestorage.googleapis.com/v0/b/facespot-b3ab6.appspot.com/o/default_img.svg?alt=media&token=8bf92214-6896-465f-97ce-125b5902691d'
+export let scale: number
+export let marginTop: number
+
+const preview = (imgPath) => {
     // @ts-ignore
     let img = new Image();
     img.src = imgPath;
@@ -15,20 +15,20 @@ const drawImage = (imgPath) => {
         // @ts-ignore
         canvas.height = container.clientHeight
 
-        let scale: number
         if (img.height <= img.width) {
             scale = canvas.clientWidth / img.width
         } else {
             scale = canvas.clientHeight / img.height
         }
 
-        const width = scale * img.width
+        const width: number = scale * img.width
         const marginLeft: number = (canvas.clientWidth - width) / 2
 
-        const height = scale * img.height
-        const marginTop: number = (canvas.clientHeight - height) / 2
+        const height: number = scale * img.height
+        marginTop = (canvas.clientHeight - height) / 2
 
         context.setTransform(scale, 0, 0, scale, marginLeft, marginTop)
+
         context.drawImage(img, 0, 0)
     })
 }
@@ -40,11 +40,11 @@ document.querySelector('.file')
         reader.readAsDataURL(img);
 
         reader.addEventListener('load', () => {
-            drawImage(reader.result)
+            preview(reader.result)
         })
     });
 
 
 window.addEventListener("load", () => {
-    drawImage(defaultImgPath)
+    preview(defaultImgPath)
 })
