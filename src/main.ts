84099ab5703cdb19b25a3a8de7cf138acdd8ef31
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-07-11 10:08:17 +0900

    [ADD] STARTボタンをクリックで画像情報をfunctionsのAPIに送信

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import axios from 'axios'
+
 // css
 import './css/style.scss'
 import './css/lineAnimation.scss'
@@ -5,3 +8,22 @@ import './css/lineAnimation.scss'
 import './js/inputImgPreview'
 import './js/lineAnimation'
 
+
+document.querySelector('.start-button')
+    .addEventListener('click', async () => {
+        const canvas = document.querySelector('.canvas');
+        const blob = await makeBlob(canvas);//
+        const url = 'https://asia-northeast1-facespot-b3ab6.cloudfunctions.net/fetchFaceInfo'
+        const header = {headers: {'Content-Type': 'application/octet-stream',}}
+
+        await axios.post(url, blob, header)
+            .then(response => console.log(response.data))
+            .catch(e => console.log(e))
+    });
+
+
+const makeBlob = (canvas) => {
+    return new Promise(resolve => {
+        canvas.toBlob(result => resolve(result));
+    });
+};
