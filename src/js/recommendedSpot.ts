d8bc19ea5c9aae3a8ca172c432d063907706584c
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-10-07 00:51:37 +0900

    [Add] spot情報の雛形を作成

---
+++ src/js/recommendedSpot.ts
@@ -0,0 +1,35 @@
+// ①年齢を分ける ageCategory (30歳以下＝１ 以上=2)
+// ②人数を3段階に分ける peopleCategory (1人＝１　2人＝2　3人以上＝3)
+// ③ genderCategory (男性のみ＝1,女性のみ=2,男女=3 )
+// ④表情
+let ageCategory = 1
+let emotion = 1
+
+
+const recommendedSpot = (ageCategory) => {
+  if (ageCategory === 1) {
+    // continue
+  }
+
+  if (emotion === 1) {
+    // continue
+  }
+
+  switch (emotion) {
+    case 1:
+      console.log(2);
+      break
+    case 2:
+      console.log(2);
+      break
+
+    default:
+      console.log(1);
+  }
+
+  console.log(1)
+
+}
+
+
+
